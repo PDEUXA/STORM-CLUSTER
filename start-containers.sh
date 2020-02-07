e2db680df1b87db8fde0c20590244033da748787
pa <auque@etud.insa-toulouse.fr>
2020-02-07 10:26:24 +0100

    Final version with data

--- start-containers.sh
+++ start-containers.sh
@@ -1,6 +1,7 @@
 
 # METTRE LE CHEMIN VERS LE JAR DE VOTRE TOPOLOGIE
-SHARED=/Users/p2a/IdeaProjects/
+SHARED=/home/pa/Documents/tpstorm/STORM-CLUSTER-master/out/artifacts/
+
 
 docker network create haginet
 
@@ -14,8 +15,27 @@ docker run -d --restart always --net haginet --name zookeeper --link nimbus:nimb
 
 #####################
 CONTNAME=supervisor
-docker run -d --restart always --net haginet --name supervisor --link zookeeper:zookeeper --link nimbus:nimbus -v $SHARED:/root/jars storm storm supervisor
+docker run -d --restart always --cpus="1.0" --net haginet --name supervisor --link zookeeper:zookeeper --link nimbus:nimbus -v $SHARED:/root/jars storm storm supervisor
 #####################
+
+#####################
+CONTNAME=supervisor2
+docker run -d --restart always --cpus="1.0" --net haginet --name supervisor2 --link zookeeper:zookeeper --link nimbus:nimbus -v $SHARED:/root/jars storm storm supervisor
+#####################
+
+#####################
+CONTNAME=supervisor3
+docker run -d --restart always --cpus="1.0"  --net haginet --name supervisor3 --link zookeeper:zookeeper --link nimbus:nimbus -v $SHARED:/root/jars storm storm supervisor
+#####################
+
+#####################
+CONTNAME=supervisor4
+docker run -d --restart always --cpus="1.0"  --net haginet --name supervisor4 --link zookeeper:zookeeper --link nimbus:nimbus -v $SHARED:/root/jars storm storm supervisor
+#####################
+
+
+
+
 
 CONTNAME=UI
 docker run -d -p 8080:8080 --restart always --net haginet --name ui --link nimbus:nimbus storm storm ui
